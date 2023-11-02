2cf89d9ea835e9b60c0ea648c4e767d5e2f52394
WolfyCodek <45407550+WolfyCodeK@users.noreply.github.com>
2023-11-02 01:57:10 +0000

    Added stats folder and add hours label to time.

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ csvPath = f'stats/pot({startingPot}).csv'
 with open(csvPath, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',',
                             quotechar=' ', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['Pot', 'Bet', 'Winrate', 'Time', 'Profit'])
+    writer.writerow(['Pot', 'Bet', 'Winrate', 'Time(hours)', 'Profit'])
 
 while (startingBet < startingPot):
     winTally = 0
@@ -54,8 +54,6 @@ while (startingBet < startingPot):
         lossStreak = 0
         profit = 0
         i = 1
-
-        #print(f'{i}: pot({pot}), bet({startingBet}), lossStreak({lossStreak})')
 
         # Play Roulette
         while ((i < (60 / spinMinutes) or lossStreak > 0) and pot > 0):
@@ -75,18 +73,13 @@ while (startingBet < startingPot):
                 lossStreak += 1
                 pot -= bet
                 
-            #print(f'{i}: pot({round(pot, 2)}), bet({bet}), lossStreak({lossStreak})')
-                
         # Print Results
         hours = round((i * spinMinutes) / 60, 3)
-        #print(f'Time played: {hours} hours ')
         
         if (pot < startingPot): 
             lossTally += 1
-            #print(f'Probability Loss: {round((1 / pow(2, lossStreak)) * 100, 3)}')
         else:
             profit = round(profit, 2)
-            #print(f'Profit: ({profit})')
             
             winTally += 1
             totalProfit += profit
@@ -98,10 +91,6 @@ while (startingBet < startingPot):
     winRate = round(winTally / (totalGamesPlayed) * 100, 3)
     averageTimePlayed = round((totalPlayTime / winTally), 2)
     averageProfit = round((totalProfit / winTally), 2)
-    #print("")
-    #print(f'Win Rate: {winRate}%')
-    #print(f'Average profit: {averageProfit}')
-    #print(f'Average time played: {averageTimePlayed}')
 
     # Write to CSV
     with open(csvPath, 'a', newline='') as csvfile:
